5cfc132963453e190b96a03551aa200dc26e35ac
Solpostgit <begemot_off@mail.ru>
2023-08-09 21:13:27 +0300

    Completed task1

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,14 @@ public class Main {
     }
     public static void task1 () {
         System.out.println("task 1");
-
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (clientOS == 1)
+            System.out.println("Установите версию приложения для Android по ссылке");
+        else
+            System.out.println("Используйте телефон с iOS или Android");
+        System.out.println();
     }
 }
