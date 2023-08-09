e0a20f63b4698cc7f244c490c790154a453c4f13
Solpostgit <begemot_off@mail.ru>
2023-08-09 21:28:02 +0300

    Added task2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        task1 (); /*
-        task2 ();
+        task1 ();
+        task2 ();/*
         task3 ();
         task4 ();
         task5 (); */
@@ -15,6 +15,26 @@ public class Main {
         }
         else if (clientOS == 1)
             System.out.println("Установите версию приложения для Android по ссылке");
+        else
+            System.out.println("Используйте телефон с iOS или Android");
+        System.out.println();
+    }
+    public static void task2 () {
+        System.out.println("task 2");
+        int clientOS = 0;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+            else System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+            else System.out.println("Установите версию приложения для Android по ссылке");
+        }
         else
             System.out.println("Используйте телефон с iOS или Android");
         System.out.println();
